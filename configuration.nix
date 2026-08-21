5d67da26d3810624fde52d5433471c490defca3a
justinroche <justinroche03@gmail.com>
2026-08-20 22:48:26 -0500

    feat(ssh): configure ssh agent

--- configuration.nix
+++ configuration.nix
@@ -73,6 +73,8 @@
   };
 
   programs.firefox.enable = true;
+  programs.ssh.startAgent = true;
+  programs.ssh.askPassword = "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
