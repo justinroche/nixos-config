e99aa951da939d8f432b51d7b1b839fb4ea491c0
justinroche <justinroche03@gmail.com>
2026-09-23 19:31:59 -0500

    refactor: remove redundant sub

--- configuration.nix
+++ configuration.nix
@@ -47,7 +47,6 @@
     auto-optimise-store = true;
     experimental-features = ["nix-command" "flakes"];
     trusted-users = ["root" "justin"];
-    substituters = ["https://devenv.cachix.org"];
     trusted-public-keys = ["devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="];
   };
 
